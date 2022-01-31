2124aef50a31047216c7d1adb35c87bab0e3787f
Victor <victormeunier.dev@gmail.com>
2022-01-31 10:22:11 +0100

    update example

--- test_scene.gd
+++ test_scene.gd
@@ -7,3 +7,10 @@ func _on_Run_pressed() -> void:
 	var res = $"VBoxContainer/HBoxContainer/LineEdit+".run_regex()
 	print(res)
 	$VBoxContainer/HBoxContainer2/Result.text = str(res)
+
+
+func _on_LineEdit_regex_result(result, txt) -> void:
+	if !result:
+		$"VBoxContainer/HBoxContainer3/LineEdit+".set("custom_colors/font_color", Color(1.0, 0, 0))
+	else:
+		$"VBoxContainer/HBoxContainer3/LineEdit+".set("custom_colors/font_color", Color(1.0, 1.0, 1.0))
